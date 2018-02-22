652e2c9a507db8772f46ece3327607ed0215e355
itomaldonado <mo.maldonado@gmail.com>
2018-02-22 15:40:20 -0500

    changes for clock

--- recitations/rec4/pi.cpp
+++ recitations/rec4/pi.cpp
@@ -23,11 +23,13 @@ int main(int argc, char **argv)
 {
   long int i, intervals = INTERVALS;
   double x, dx, f, sum, pi;
-  double time2;
+  double time2, time4;
 #ifdef _OPENMP
   double time1 = omp_get_wtime();
+  time_t time3 = clock();
 #else
   time_t time1 = clock();
+  time_t time3 = clock();
 #endif
 
   printf("Number of intervals: %ld\n", intervals);
@@ -50,10 +52,13 @@ int main(int argc, char **argv)
 #else
   time2 = (clock() - time1) / (double) CLOCKS_PER_SEC;
 #endif
+time4 = (clock() - time3) / (double) CLOCKS_PER_SEC;
+
 
   printf("Computed PI %.24f\n", pi);
   printf("The true PI %.24f\n\n", PI25DT);
   printf("Elapsed time (s) = %.2lf\n", time2);
+  printf("Clock() Elapsed time (s) = %.2lf\n", time4);
 
   return 0;
 }
