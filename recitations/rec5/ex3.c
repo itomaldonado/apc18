d9586a412bd60cdfbfb5c24746e6686073d542c9
itomaldonado <mo.maldonado@gmail.com>
2018-03-08 13:19:45 -0500

    ex3 using scatterv and gatherv

--- recitations/rec5/ex3.c
+++ recitations/rec5/ex3.c
@@ -6,9 +6,9 @@
 
 int main(int argc, char *argv[])
 {
-    int i, my_id, num_procs, num_elem;
+    int i, my_id, num_procs, num_elem, stride, sum;
     int array[N], array_final[N];
-    int *array_recv;
+    int *array_recv, *offsets, *counts; 
 
     MPI_Init(&argc, &argv);                    /* starts MPI */
     MPI_Comm_rank(MPI_COMM_WORLD, &my_id);     /* get current process id */
@@ -26,21 +26,39 @@ int main(int argc, char *argv[])
     }
 
     // Scatter array
-    array_recv = (int *)malloc(sizeof(int) * num_elem);
-    MPI_Scatter(&array, num_elem, MPI_INT, array_recv, num_elem, MPI_INT, 0, MPI_COMM_WORLD);
+    offsets = (int *)malloc(num_procs*sizeof(int));
+    counts = (int *)malloc(num_procs*sizeof(int));
+
+    // get counts for each rank
+    for (i=0; i < num_procs; ++i) { 
+        counts[i] = N / num_procs;
+    }
+    for (i = 0; i < N % num_procs; i++){
+        // add one to each rank until we use the reminder
+        counts[i] += 1;
+    }
+
+    // get the offsets of each rank
+    sum = 0
+    for (i = 0; i < num_procs; i++) {
+        offsets[i] = sum;
+        sum += counts[i];
+    }
+
+    array_recv = (int *)malloc(sizeof(int) * counts[my_id]);
+    MPI_Scatterv(&array, counts, offsets, MPI_INT, array_recv, counts[my_id], MPI_INT, 0, MPI_COMM_WORLD);
+    
+    //MPI_Scatter(&array, num_elem, MPI_INT, array_recv, num_elem, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Overwrite array with rank id
-    for(i = 0; i < num_elem; i++) {
+    for(i = 0; i < counts[my_id]; i++) {
         array_recv[i] = my_id;
     }
 
     // Gather data
     int *sub_avgs = NULL;
-    if (my_id == 0) {
-        MPI_Gather(array_recv, num_elem, MPI_INT, &array_final, num_elem, MPI_INT, 0, MPI_COMM_WORLD);
-    } else {
-        MPI_Gather(array_recv, num_elem, MPI_INT, NULL, num_elem, MPI_INT, 0, MPI_COMM_WORLD);
-    }
+    MPI_Gatherv(array_recv, counts[my_id], MPI_INT, &array_final, counts, offsets, MPI_INT, 0, MPI_COMM_WORLD);
+    //MPI_Gather(array_recv, num_elem, MPI_INT, &array_final, num_elem, MPI_INT, 0, MPI_COMM_WORLD);
 
     if (my_id == 0) {
         for(i = 0; i < N;i++) {
@@ -50,6 +68,8 @@ int main(int argc, char *argv[])
 
     // clean up
     free(array_recv);
+    free(offsets);
+    free(counts);
 
     // finalize
     MPI_Barrier(MPI_COMM_WORLD);
