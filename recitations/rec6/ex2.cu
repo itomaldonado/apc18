5d1f3fb089f2f4bbd59e0244f7cedcf23920a99f
itomaldonado <mo.maldonado@gmail.com>
2018-05-01 12:25:04 -0400

    transposing single per thread

--- recitations/rec6/ex2.cu
+++ recitations/rec6/ex2.cu
@@ -2,12 +2,34 @@
 #include <stdio.h>
 #include <math.h>
 
+// kernels transpose a tile of TILE_DIM x TILE_DIM elements
+// using a TILE_DIM x BLOCK_ROWS thread block, so that each thread
+// transposes TILE_DIM/BLOCK_ROWS elements.
+// TILE_DIM must be an integral multiple of BLOCK_ROWS
+#define TILE_DIM 32
+#define BLOCK_ROWS 8
 
-__global__ void cuTranspose(int rows, int cols, float *input, float *output)
-{
-  
-    // Write your transpose kernel here
-    return;
+
+__global__ void cuTranspose(int rows, int cols, const float *input, float *output) {
+
+  //int x = blockIdx.x * TILE_DIM + threadIdx.x;
+  //int y = blockIdx.y * TILE_DIM + threadIdx.y;
+  //int width = gridDim.x * TILE_DIM;
+
+  int tile_size = blockDim.x ;
+  int column = tile_size * blockIdx.x + threadIdx.x;
+  int row = tile_size * blockIdx.y + threadIdx.y;
+  if(column < cols && row < rows){
+    output[column*cols + row] = input[column + row*rows];
+  }
+
+  /*
+  for (int j = 0; j < TILE_DIM; j+= BLOCK_ROWS) {
+    output[x*width + (y+j)] = input[(y+j)*width + x];
+  }
+  */
+
+  return;
 }
 
 int check(int nrows, int ncols, float* input, float* output) {
@@ -30,6 +52,9 @@ int main(void)
 
   float *input, *output;
 
+  dim3 dimGrid(r/TILE_DIM, c/TILE_DIM, 1);
+  dim3 dimBlock(TILE_DIM, BLOCK_ROWS, 1);
+
   // Allocate Unified Memory – accessible from CPU or GPU
   cudaMallocManaged(&input, N*sizeof(float));
   cudaMallocManaged(&output, N*sizeof(float));
@@ -37,14 +62,16 @@ int main(void)
   // initialize input and output matrices on the host
   for (int i = 0; i < r; i++) {
       for(int j = i; j < c; j++) {
-        input[i*c + j] = (float)nrows*i + r*1.0f;
+        input[i*c + j] = (float)r*i + r*1.0f;
         output[i*c + j] = 5.0f;
       }
 
   }
 
-//   Transpose the matrix
-  cuTranspose<<<1, 1>>>(r, c, input, output);
+  // Transpose the matrix
+  printf("Before\n");
+  cuTranspose<<<dimGrid, dimBlock>>>(r, c, input, output);
+  printf("After\n");
 
   // Wait for GPU to finish before accessing on host
   cudaDeviceSynchronize();
